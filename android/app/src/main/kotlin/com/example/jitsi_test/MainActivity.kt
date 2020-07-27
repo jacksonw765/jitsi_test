58d9ee17b3f144de0e150a50e9a029c6ebe395fd
Jackson Welch Ryzentosh <jacksonw765@gmail.com>
2020-07-26 23:37:51 -0400

    test

--- android/app/src/main/kotlin/com/example/jitsi_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.jitsi_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,2 @@
+// Agora AppId
+const APP_ID = 'bc96c642dd8a4415bde53f78c1b38e8e';
